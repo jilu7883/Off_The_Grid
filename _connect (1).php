e9344619a58e247644f6000f4778f25bd8be9d40
Regina <jilu7883@colorado.edu>
2018-04-16 20:24:28 -0600

    Delete _connect (1).php

--- _connect (1).php
+++
@@ -1,8 +0,0 @@
-<?php
-//Enter the connection details of your MySQL database here
-$db_host="localhost";
-$db_user="id4832378_offthegrid";
-$db_password="12345";
-$db_name="id4832378_offthegridcapstone";
-
-?>
